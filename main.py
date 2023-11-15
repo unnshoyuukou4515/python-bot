d0b9416d955aec8fdf37ad72204b5708b339c5da
Koichiro Yokoi <112148201+unnshoyuukou4515@users.noreply.github.com>
2023-11-15 13:19:28 +0900

    chage it back

--- main.py
+++ main.py
@@ -26,71 +26,46 @@ handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    user_id = event.source.user_id
-    text = event.message.text
-    user_requests[user_id] = text
+  text = event.message.text
+  if '天気' in text:
+    line_bot_api.reply_message(
+      event.reply_token,
+      [
+      TextSendMessage(text='位置情報を教えてください。'),
+      TextSendMessage(text='line://nv/location')
+      ]
+    )
 
+@handler.add(MessageEvent, message=LocationMessage)
+def handle_message(event):
+    text = event.message.text
     if '天気' in text:
-        messaging_api.reply_message(
+        line_bot_api.reply_message(
             event.reply_token,
             [
-                TextSendMessage(text='Your location Please'),
+                TextSendMessage(text='位置情報を教えてください。'),
                 TextSendMessage(text='line://nv/location')
             ]
         )
-    elif 'weather' in text:
-        result = sc.get_weather_from_english()
-        messaging_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=result)
-        )
-    elif 'CC' in text:
-        messaging_api.reply_message(
-            event.reply_token,
-            [TextSendMessage(text='Your location Please'), 
-             TextSendMessage(text='line://nv/location')]
-        )
     else:
-        messaging_api.reply_message(
+        # その他のテキストメッセージに対する処理
+        line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=event.message.text)
         )
-
-CC_location = (35.6547486111, 139.7307916667)
-
-@handler.add(MessageEvent, message=LocationMessage)
-def handle_location(event):
-    user_id = event.source.user_id
-    user_location = (event.message.latitude, event.message.longitude)
-    result = "Failed to process request"
-
-    if user_requests.get(user_id) == '天気':
-        result = sc.get_weather_from_location_JP(event.message.address)
-    elif user_requests.get(user_id) == 'CC':
-        distance = geodesic(user_location, CC_location).kilometers
-        result = f'You are {distance:.2f} km away from Code Chrysalis.'
-
-    messaging_api.reply_message(
-    event.reply_token,
-    [TextSendMessage(text=result)]
-)
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
